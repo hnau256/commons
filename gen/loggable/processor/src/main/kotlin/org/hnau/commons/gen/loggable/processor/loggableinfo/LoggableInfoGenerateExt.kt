10ea7a6fcd59183112e6929507517901f8bb3e15
Mark Zorikhin <hnau256@gmail.com>
2026-02-28 22:50:56 +0200

    loggable update

--- gen/loggable/processor/src/main/kotlin/org/hnau/commons/gen/loggable/processor/loggableinfo/LoggableInfoGenerateExt.kt
+++ gen/loggable/processor/src/main/kotlin/org/hnau/commons/gen/loggable/processor/loggableinfo/LoggableInfoGenerateExt.kt
@@ -6,46 +6,58 @@ import com.squareup.kotlinpoet.ClassName
 import com.squareup.kotlinpoet.FileSpec
 import com.squareup.kotlinpoet.FunSpec
 import com.squareup.kotlinpoet.KModifier
-import com.squareup.kotlinpoet.MemberName
+import com.squareup.kotlinpoet.ParameterizedTypeName.Companion.parameterizedBy
 import com.squareup.kotlinpoet.PropertySpec
 import org.hnau.commons.kotlin.ifTrue
 
-internal fun LoggableInfo.generateCode(
-    codeGenerator: CodeGenerator,
-) {
+internal fun LoggableInfo.generateCode(codeGenerator: CodeGenerator) {
 
-    val getter = FunSpec
-        .getterBuilder()
-        .addStatement(
-            "return %M.withTag(%S)",
-            MemberName("co.touchlab.kermit", "Logger"),
-            tag,
+    val initializerVarName = "${className}LogInitializer"
+
+    val staticLoggerInitializerProperty = PropertySpec
+        .builder(
+            name = initializerVarName,
+            type = initializerClassName.parameterizedBy(loggerClassName),
+            modifiers = listOf(KModifier.PRIVATE),
         )
+        .initializer("%T()", initializerClassName)
         .build()
 
-    val property = PropertySpec
+    val loggerProperty = PropertySpec
         .builder(
-            name = "log",
-            type = ClassName("co.touchlab.kermit", "Logger"),
+            name = "logger",
+            type = loggerClassName,
             modifiers = listOf(KModifier.INTERNAL),
         )
         .receiver(
             ClassName(
                 packageName = classPackage,
-                simpleNames = listOfNotNull(
-                    className,
-                    hasCompanion.ifTrue { "Companion" },
-                ),
+                simpleNames =
+                    listOfNotNull(
+                        className,
+                        hasCompanion.ifTrue { "Companion" },
+                    ),
             )
         )
-        .getter(getter)
+        .getter(
+            FunSpec
+                .getterBuilder()
+                .addStatement(
+                    "return %N { %T.withTag(%S) }",
+                    initializerVarName,
+                    loggerClassName,
+                    tag,
+                )
+                .build()
+        )
         .build()
 
     val fileName = "${className}Logger"
 
     val fileSpec = FileSpec
         .builder(classPackage, fileName)
-        .addProperty(property)
+        .addProperty(staticLoggerInitializerProperty)
+        .addProperty(loggerProperty)
         .build()
 
     codeGenerator
@@ -57,3 +69,9 @@ internal fun LoggableInfo.generateCode(
         .bufferedWriter()
         .use(fileSpec::writeTo)
 }
+
+private val loggerClassName: ClassName =
+    ClassName("co.touchlab.kermit", "Logger")
+
+private val initializerClassName: ClassName =
+    ClassName("org.hnau.commons.kotlin.lazy", "Initializer")
