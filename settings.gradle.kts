a0fcd0d87a294f37424fc571a928ec7ab73c921e
Mark Zorikhin <hnau256@gmail.com>
2026-03-06 19:45:43 +0200

    use plugins from maven repository

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,6 +1,6 @@
 pluginManagement {
-    includeBuild("../plugins")
     repositories {
+        mavenLocal()
         mavenCentral()
         google()
         gradlePluginPortal()
@@ -9,7 +9,7 @@ pluginManagement {
 
 plugins {
     id("org.gradle.toolchains.foojay-resolver-convention") version "1.0.0"
-    id("org.hnau.settings") version "0.1.0"
+    id("org.hnau.settings") version "1.0.0"
 }
 
 rootProject.name = "commons"
