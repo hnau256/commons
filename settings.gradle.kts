fcb9e48e280746018e1dc056e51d227ecc857147
Mark Zorikhin <hnau256@gmail.com>
2026-03-18 08:57:04 +0200

    v1.4.3

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -10,13 +10,13 @@ pluginManagement {
 }
 
 plugins {
-    id("org.hnau.plugin.settings") version "1.2.11"
+    id("org.hnau.plugin.settings") version "1.2.12"
 }
 
 hnau {
     groupId = "org.hnau.commons"
     publish {
-        version = "1.4.2"
+        version = "1.4.3"
         gitUrl = "https://github.com/hnau256/commons"
     }
 }
