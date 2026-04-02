5123b9868b904350acc411a04bbefba1ffcf62bb
Mark Zorikhin <hnau256@gmail.com>
2026-04-02 15:31:54 +0300

    Cbor.toMapper

--- kotlin/src/commonMain/kotlin/org/hnau/commons/kotlin/mapper/CborMapper.kt
+++ kotlin/src/commonMain/kotlin/org/hnau/commons/kotlin/mapper/CborMapper.kt
@@ -1,12 +1,11 @@
 package org.hnau.commons.kotlin.mapper
 
 import kotlinx.serialization.KSerializer
-import kotlinx.serialization.json.Json.Default.decodeFromString
-import kotlinx.serialization.json.Json.Default.encodeToString
+import kotlinx.serialization.cbor.Cbor
 
 fun <T> Cbor.toMapper(
     serializer: KSerializer<T>,
-): Mapper<String, T> = Mapper(
-    direct = { json -> decodeFromString(serializer, json) },
-    reverse = { value -> encodeToString(serializer, value) },
+): Mapper<ByteArray, T> = Mapper(
+    direct = { bytes -> decodeFromByteArray(serializer, bytes) },
+    reverse = { value -> encodeToByteArray(serializer, value) },
 )
