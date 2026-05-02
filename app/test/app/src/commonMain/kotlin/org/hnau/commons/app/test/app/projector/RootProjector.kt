7cb58660f4fbba79e4790bd8d93eb587385bf905
Mark Zorikhin <hnau256@gmail.com>
2026-05-02 11:57:44 +0300

    Update `versionCode` and adjust icon size in `RootProjector`
    - Increment `versionCode` to 109 in `version.properties`.
    - Set a fixed height of `64.dp` for the "Clear" icon within the `RootProjector` to improve its touch target and visibility.

--- app/test/app/src/commonMain/kotlin/org/hnau/commons/app/test/app/projector/RootProjector.kt
+++ app/test/app/src/commonMain/kotlin/org/hnau/commons/app/test/app/projector/RootProjector.kt
@@ -4,7 +4,9 @@ import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Clear
 import androidx.compose.material.icons.filled.Settings
@@ -12,6 +14,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import arrow.core.Ior
 import kotlinx.coroutines.CoroutineScope
 import org.hnau.commons.app.model.theme.palette.Palettes
@@ -77,7 +80,7 @@ class RootProjector(
                                             {
                                                 Icon(
                                                     icon = Icons.Default.Clear,
-                                                    modifier = Modifier.clickable {
+                                                    modifier = Modifier.height(64.dp).clickable {
                                                         value.value = "".toEditingString()
                                                     }
                                                 )
