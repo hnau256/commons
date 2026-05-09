28cc510f573ea8c003dcc44c1f4f483a6789e1e6
Mark Zorikhin <hnau256@gmail.com>
2026-05-09 10:18:02 +0300

    Add `@JvmName` to `Either` extension properties in `EitherExt.kt`
    - Add `@JvmName` annotations to the `value` property getters for `Either<Nothing, T>` and `Either<T, Nothing>` to prevent JVM signature clashes.

--- kotlin/src/commonMain/kotlin/org/hnau/commons/kotlin/EitherExt.kt
+++ kotlin/src/commonMain/kotlin/org/hnau/commons/kotlin/EitherExt.kt
@@ -1,8 +1,10 @@
 package org.hnau.commons.kotlin
 
 import arrow.core.Either
+import kotlin.jvm.JvmName
 
 val <T> Either<Nothing, T>.value: T
+    @JvmName("getEitherWitchLeftIsNothingValue")
     get() = fold(
         ifLeft = { throwThisShouldNeverHappenError() },
         ifRight = ::it,
@@ -10,6 +12,7 @@ val <T> Either<Nothing, T>.value: T
 
 
 val <T> Either<T, Nothing>.value: T
+    @JvmName("getEitherWitchRightIsNothingValue")
     get() = fold(
         ifLeft = ::it,
         ifRight = { throwThisShouldNeverHappenError() },
