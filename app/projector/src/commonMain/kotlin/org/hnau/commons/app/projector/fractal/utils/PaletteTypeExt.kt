4c34522b58de5ee21e56109148a2e6d60a91ae56
Mark Zorikhin <hnau256@gmail.com>
2026-05-25 16:45:20 +0300

    Refactor `PaletteType` resolution logic and remove deprecated extensions
    - Update `PaletteType.Companion.resolve` to return `PaletteType.Error` when saturation is neutral if the mood is `Error`.
    - Remove deprecated `orInactive` and `orError` extension functions from `PaletteType`.
    - Remove unused `foldBoolean` import.

--- app/projector/src/commonMain/kotlin/org/hnau/commons/app/projector/fractal/utils/PaletteTypeExt.kt
+++ app/projector/src/commonMain/kotlin/org/hnau/commons/app/projector/fractal/utils/PaletteTypeExt.kt
@@ -1,29 +1,19 @@
 package org.hnau.commons.app.projector.fractal.utils
 
 import org.hnau.commons.app.model.theme.palette.PaletteType
-import org.hnau.commons.kotlin.foldBoolean
-
-@Deprecated("Use Mood and Saturation instead")
-fun PaletteType.orInactive(
-    active: Boolean,
-): PaletteType = active.foldBoolean(
-    ifTrue = { this },
-    ifFalse = { PaletteType.Neutral }
-)
-
-@Deprecated("Use Mood and Saturation instead")
-fun PaletteType.orError(
-    error: Boolean,
-): PaletteType = error.foldBoolean(
-    ifTrue = { PaletteType.Error },
-    ifFalse = { this }
-)
 
 fun PaletteType.Companion.resolve(
     mood: Mood,
     saturation: Saturation,
 ): PaletteType = saturation.fold(
-    ifNeutral = { PaletteType.Neutral },
+    ifNeutral = {
+        mood.fold(
+            ifPrimary = { PaletteType.Neutral },
+            ifSecondary = { PaletteType.Neutral },
+            ifTertiary = { PaletteType.Neutral },
+            ifError = { PaletteType.Error }
+        )
+    },
     ifActive = {
         mood.fold(
             ifPrimary = { PaletteType.Primary },
